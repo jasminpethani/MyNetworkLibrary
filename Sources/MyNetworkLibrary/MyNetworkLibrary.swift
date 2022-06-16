cec2d47d32b4ce34ec975f8ddb66af5699eefaed
Jasmin Pethani <jasminp@backbase.com>
2022-06-16 13:53:01 +0530

    Initial Commit

---
+++ Sources/MyNetworkLibrary/MyNetworkLibrary.swift
@@ -0,0 +1,6 @@
+public struct MyNetworkLibrary {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/MyNetworkLibraryTests/MyNetworkLibraryTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import MyNetworkLibrary
+
+final class MyNetworkLibraryTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(MyNetworkLibrary().text, "Hello, World!")
+    }
+}
