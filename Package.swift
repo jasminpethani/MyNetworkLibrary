fe1d6f8405b0f80a9b669029b9a099a8d172e2f9
Jasmin Pethani <jasminp@backbase.com>
2022-06-16 14:01:45 +0530

    firs commit network

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "MyNetworkLibrary",
+    platforms: [.iOS("13.0")],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -21,8 +22,5 @@ let package = Package(
         .target(
             name: "MyNetworkLibrary",
             dependencies: []),
-        .testTarget(
-            name: "MyNetworkLibraryTests",
-            dependencies: ["MyNetworkLibrary"]),
     ]
 )
--- Sources/MyNetworkLibrary/MyNetworkLibrary.swift
+++ Sources/MyNetworkLibrary/MyNetworkLibrary.swift
@@ -1,6 +1,17 @@
-public struct MyNetworkLibrary {
-    public private(set) var text = "Hello, World!"
+//  Created by JASMIN PETHANI on 10/06/22 and 12:28
 
-    public init() {
+import Foundation
+import Combine
+
+public protocol NetworkserviceProtocol {
+    func fetch<M: Decodable>(url:URL, modelType: M.Type) -> AnyPublisher<M, Error>
+}
+
+public final class WebService: NetworkserviceProtocol {
+    public func fetch<M: Decodable>(url:URL, modelType: M.Type) -> AnyPublisher<M, Error> {
+        return URLSession.shared.dataTaskPublisher(for: url).map { $0.data }
+            .decode(type: modelType, decoder: JSONDecoder())
+            .receive(on: RunLoop.main)
+            .eraseToAnyPublisher()
     }
 }
